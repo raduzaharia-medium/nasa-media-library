d03b61df9783e0a3ed54d669314c9bff2bc9c295
Radu Zaharia <101859038+raduzaharia-medium@users.noreply.github.com>
2024-11-09 09:24:38 +0000

    Small changes and SVG favicon.

--- src/app.test.js
+++ src/app.test.js
@@ -3,6 +3,7 @@ import App from "./app";
 
 test("renders learn react link", () => {
   render(<App />);
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
